36fffeaf41b2751a90976ff39307147e71114e6d
Alexander Herzog <alexander.herzog@gmx.de>
2024-06-27 11:22:15 +0200

    Updated desktop app version

--- desktop-app/2_Build.bat
+++ desktop-app/2_Build.bat
@@ -1,8 +1,11 @@
 cd ..
 del MiniSimulator.exe
+del MiniSimulator_Linux_MacOS.zip
 call neu.cmd build --release
 cd desktop-app
 "C:\Program Files (x86)\NSIS\makensis.exe" Launcher.nsi
 move MiniSimulator.exe ..
 cd ..
+move .\dist\MiniSimulator-release.zip MiniSimulator_Linux_MacOS.zip
 rmdir /S /Q dist
+cd desktop-app
